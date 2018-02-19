871f08c928e1f7033eaa01b21ad0cd8533e317e3
sumeet2388 <35688043+sumeet2388@users.noreply.github.com>
2018-02-19 16:14:15 +0530

    Add files via upload

--- pro_em.py
+++ pro_em.py
@@ -1,7 +1,7 @@
 import smtplib
 s= smtplib.SMTP("smtp.gmail.com",587)
 s.starttls()
-s.login("sumeetssk.2388@gmail.com","sumeet2388")
+s.login("sumeetssk.2388@gmail.com","password")
 msg="Hii how r u..?"
 s.sendmail("sumeetssk.2388@gmail.com","sumeetssk.2388@gmail.com",msg)
 s.quit()
