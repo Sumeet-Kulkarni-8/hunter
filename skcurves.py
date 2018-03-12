7ee637bfda41f32c5b3c90925186f809afe14f9e
sumeet2388 <35688043+sumeet2388@users.noreply.github.com>
2018-03-12 16:04:53 +0530

    Add files via upload

---
+++ skcurves.py
@@ -0,0 +1,21 @@
+#!import matplotlib.pyplot as plt
+import numpy as np
+ 
+# setting the x - coordinates
+x = np.arange(0, 2*(np.pi), 0.1)
+# setting the corresponding y - coordinates
+y = np.sin(x)
+ 
+# potting the points
+plt.plot(x, y)
+ 
+# function to show the plot
+plt.show()
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 16:03:51 2018
+
+@author: ec2017b218
+"""
+
