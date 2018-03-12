5fca51dcc9eaea600b9d9410bedce4af96d17e53
sumeet2388 <35688043+sumeet2388@users.noreply.github.com>
2018-03-12 16:00:37 +0530

    Add files via upload

---
+++ skplot.py
@@ -0,0 +1,27 @@
+#!import matplotlib.pyplot as plt
+ 
+# x axis values
+x1 = [1,2,3,4,5]
+# corresponding y axis values
+y1 = [2,4,1,3,6]
+ 
+# plotting the points 
+plt.plot(x1, y1)
+ 
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+ 
+# giving a title to my graph
+plt.title('My first graph in python!')
+ 
+# function to show the plot
+plt.show()#!/usr/bin/env python3/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 15:59:30 2018
+
+@author: ec2017b218
+"""
+
